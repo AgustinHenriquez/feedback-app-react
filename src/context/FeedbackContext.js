b09388395b3d405d66b39a176d754d4773a6f7ad
Agus Henriquez <agustinhenriquez98@gmail.com>
2021-12-14 19:12:00 -0300

    Added fetching items from data base

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,30 +1,28 @@
 import { v4 as uuidv4 } from 'uuid'
-import { createContext, useState } from 'react'
+import { createContext, useState, useEffect } from 'react'
 
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState([
-    {
-      id: 1,
-      text: 'This is item 1',
-      rating: 5,
-    },
-    {
-      id: 2,
-      text: 'This is item 2',
-      rating: 7,
-    },
-    {
-      id: 3,
-      text: 'This is item 3',
-      rating: 6,
-    },
-  ])
+  const [feedback, setFeedback] = useState([])
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false,
   })
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    fetchFeedback()
+  }, [])
+
+  const fetchFeedback = async () => {
+    const response = await fetch(
+      'http://localhost:5000/feedback?_sort=is&_order=desc'
+    )
+    const data = await response.json()
+    setFeedback(data)
+    setIsLoading(false)
+  }
 
   const deleteFeedback = (id) => {
     if (window.confirm('Are you sure you want to delete?')) {
@@ -54,6 +52,7 @@ export const FeedbackProvider = ({ children }) => {
       value={{
         feedback,
         feedbackEdit,
+        isLoading,
         deleteFeedback,
         addFeedback,
         updateFeedback,
